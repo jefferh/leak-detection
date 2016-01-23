6a5ab5e61f3a0e87116e53bc7bf165d6ad604612
Jefferson Huang <hsiaobei@gmail.com>
2016-01-23 16:18:08 -0500

    added code for gbm with huber loss

--- sTests.py
+++ sTests.py
@@ -78,6 +78,12 @@ def fitModel(inSeries, outSeries, maxLag, method, threshold):
             for i in range(numInSeries):
                 coefs[i,:] = gbm.feature_importances_[i*maxLag:(i+1)*maxLag]
             coefList.append(coefs)
+        elif method=='gbmhuber':
+            gbmhuber = ensemble.GradientBoostingRegressor(loss='huber')
+            gbmhuber.fit(X_scaled, y)
+            for i in range(numInSeries):
+                coefs[i,:] = gbmhuber.feature_importances_[i*maxLag:(i+1)*maxLag]
+            coefList.append(coefs)            
         # Get the maximal coefficient, and the corresponding lag, for each input 
         # series
         maxCoefs = np.zeros(numInSeries)
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
+import time
 import numpy as np
 import sgenRandomized as sgr
 import sTests as st
 
+start_time = time.time()
+
 # Get stock data
 sd = sgr.getStockData(1500)
 # Generate 10 time series based on the stock data
-(scaledStocks, genSeries, dep, funcs) = sgr.generateSeries(sd, numGenSeries=10, numDataPts=1000, maxLag=20, maxSummands=3, maxFactors=3, noiseStDev=0.25)
+(scaledStocks, genSeries, dep, funcs) = sgr.generateSeries(sd, numGenSeries=20, numDataPts=1000, maxLag=20, maxSummands=3, maxFactors=3, noiseStDev=0.25)
 
 # Evaluate LASSO
 depFitted_lasso = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='lassocv', threshold=0.2)
@@ -24,3 +27,11 @@ depFitted_gbm = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=2
 (relSeries, idSeries_gbm, F1_scores_gbm) = st.computeF1Score(dep, depFitted_gbm)
 print "GBM mean F1-score: " + str(np.nanmean(F1_scores_gbm))
 print "GBM standard error of mean F1-score: " + str(np.nanstd(F1_scores_gbm)/np.count_nonzero(~np.isnan(F1_scores_gbm)))
+
+# Evaluate GBM with Huber Loss
+depFitted_gbmhuber = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='gbmhuber', threshold=0.2)
+(relSeries, idSeries_gbmhuber, F1_scores_gbmhuber) = st.computeF1Score(dep, depFitted_gbmhuber)
+print "GBM w/ Huber Loss mean F1-score: " + str(np.nanmean(F1_scores_gbmhuber))
+print "GBM w/ Huber Loss standard error of mean F1-score: " + str(np.nanstd(F1_scores_gbmhuber)/np.count_nonzero(~np.isnan(F1_scores_gbmhuber)))
+
+print("Time elapsed: %s seconds" % (time.time() - start_time))
