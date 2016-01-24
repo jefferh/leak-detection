eeb75de200b45f4147ca024b246699b7190416e2
Jefferson Huang <hsiaobei@gmail.com>
2016-01-24 15:18:47 -0500

    added code to write generated data to file, and to read it back

--- test.py
+++ test.py
@@ -4,14 +4,35 @@ import sgenRandomized as sgr
 import sTests as st
 
 # Get stock data
+start_time = time.time()
 sd = sgr.getStockData(1500)
 # Generate 10 time series based on the stock data
 (scaledStocks, genSeries, dep, funcs) = sgr.generateSeries(sd, numGenSeries=20, numDataPts=1000, maxLag=20, maxSummands=3, maxFactors=3, noiseStDev=0.25)
+print "================================="
+print("Time to generate data: %s seconds" % (time.time() - start_time))
+# Write the generated data
+start_time = time.time()
+np.save('scaledStocks', scaledStocks)
+np.save('genSeries', genSeries)
+np.save('dep', dep)
+np.save('funcs', funcs)
+print "================================="
+print("Time to write data to file: %s seconds" % (time.time() - start_time))
+
+# Read the generated data
+start_time = time.time()
+scaledStocks = np.load('scaledStocks.npy')
+genSeries = np.load('genSeries.npy')
+dep = np.load('dep.npy')
+funcs = np.load('funcs.npy')
+print "================================="
+print("Time to read data from file: %s seconds" % (time.time() - start_time))
 
 # Evaluate LASSO
 start_time = time.time()
 depFitted_lasso = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='lassocv', threshold=0.2)
 (relSeries, idSeries_lasso, F1_scores_lasso) = st.computeF1Score(dep, depFitted_lasso)
+print "================================="
 print "LASSO mean F1-score: " + str(np.nanmean(F1_scores_lasso))
 print "LASSO standard error of mean F1-score: " + str(np.nanstd(F1_scores_lasso)/np.count_nonzero(~np.isnan(F1_scores_lasso)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
@@ -20,6 +41,7 @@ print("Time elapsed: %s seconds" % (time.time() - start_time))
 start_time = time.time()
 depFitted_elasticnet = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='elasticnetcv', threshold=0.2)
 (relSeries, idSeries_elasticnet, F1_scores_elasticnet) = st.computeF1Score(dep, depFitted_elasticnet)
+print "================================="
 print "Elastic Net F1-score: " + str(np.nanmean(F1_scores_elasticnet))
 print "Elastic Net standard error of mean F1-score: " + str(np.nanstd(F1_scores_elasticnet)/np.count_nonzero(~np.isnan(F1_scores_elasticnet)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
@@ -28,6 +50,7 @@ print("Time elapsed: %s seconds" % (time.time() - start_time))
 start_time = time.time()
 depFitted_gbm = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='gbm', threshold=0.2)
 (relSeries, idSeries_gbm, F1_scores_gbm) = st.computeF1Score(dep, depFitted_gbm)
+print "================================="
 print "GBM mean F1-score: " + str(np.nanmean(F1_scores_gbm))
 print "GBM standard error of mean F1-score: " + str(np.nanstd(F1_scores_gbm)/np.count_nonzero(~np.isnan(F1_scores_gbm)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
@@ -36,6 +59,7 @@ print("Time elapsed: %s seconds" % (time.time() - start_time))
 start_time = time.time()
 depFitted_gbmhuber = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='gbmhuber', threshold=0.2)
 (relSeries, idSeries_gbmhuber, F1_scores_gbmhuber) = st.computeF1Score(dep, depFitted_gbmhuber)
+print "================================="
 print "GBM w/ Huber Loss mean F1-score: " + str(np.nanmean(F1_scores_gbmhuber))
 print "GBM w/ Huber Loss standard error of mean F1-score: " + str(np.nanstd(F1_scores_gbmhuber)/np.count_nonzero(~np.isnan(F1_scores_gbmhuber)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
@@ -44,6 +68,7 @@ print("Time elapsed: %s seconds" % (time.time() - start_time))
 start_time = time.time()
 depFitted_randomforest = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='randomforest', threshold=0.2)
 (relSeries, idSeries_randomforest, F1_scores_randomforest) = st.computeF1Score(dep, depFitted_randomforest)
+print "================================="
 print "Random Forest mean F1-score: " + str(np.nanmean(F1_scores_randomforest))
 print "Random Forest standard error of mean F1-score: " + str(np.nanstd(F1_scores_randomforest)/np.count_nonzero(~np.isnan(F1_scores_randomforest)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
@@ -52,8 +77,7 @@ print("Time elapsed: %s seconds" % (time.time() - start_time))
 start_time = time.time()
 depFitted_gaussianprocess = st.fitModel(inSeries=scaledStocks, outSeries=genSeries, maxLag=20, method='gaussianprocessARD', threshold=0.2)
 (relSeries, idSeries_gaussianprocess, F1_scores_gaussianprocess) = st.computeF1Score(dep, depFitted_gaussianprocess)
+print "================================="
 print "Gaussian Process w/ ARD mean F1-score: " + str(np.nanmean(F1_scores_gaussianprocess))
 print "Gaussian Process w/ ARD standard error of mean F1-score: " + str(np.nanstd(F1_scores_gaussianprocess)/np.count_nonzero(~np.isnan(F1_scores_gaussianprocess)))
 print("Time elapsed: %s seconds" % (time.time() - start_time))
-
-
